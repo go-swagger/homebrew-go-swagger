34cd2a1d241b4ca64e9ded09d45ddd085bbcc7dd
Ivan Porto Carrero <ivan@flanders.co.nz>
2016-10-10 00:40:35 -0700

    update for 0.7.0

--- go-swagger.rb
+++ go-swagger.rb
@@ -1,9 +1,9 @@
 class GoSwagger < Formula
   desc "Toolkit to work with swagger for golang"
   homepage "https://github.com/go-swagger/go-swagger"
-  url "https://github.com/go-swagger/go-swagger/releases/download/0.6.0/swagger_darwin_amd64"
-  version "0.6.0"
-  sha256 "bd500cebf0bef99ea5d4e2db88e2733be2f92ca5879de27fccf6454c7e7d8f9e"
+  url "https://github.com/go-swagger/go-swagger/releases/download/0.7.0/swagger_darwin_amd64"
+  version "0.7.0"
+  sha256 "2e0f5789541e870df9e715921fabb7849319982a9f3acb5a57be655612f3cacd"
 
   def install
     system "mv", "swagger_darwin_amd64", "swagger"

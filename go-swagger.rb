39ac9d1a37e3196aaa09877a0fe3aad8c70f4866
Ivan Porto Carrero <ivan@flanders.co.nz>
2019-08-19 23:43:05 -0700

    bump to v0.20.1
    Signed-off-by: Ivan Porto Carrero <ivan@flanders.co.nz>

--- go-swagger.rb
+++ go-swagger.rb
@@ -1,9 +1,9 @@
 class GoSwagger < Formula
   desc "Toolkit to work with swagger for golang"
   homepage "https://github.com/go-swagger/go-swagger"
-  url "https://github.com/go-swagger/go-swagger/releases/download/v0.20.0/swagger_darwin_amd64"
-  version "0.20.0"
-  sha256 "95f10f727a607e8a00e8766797bf97509f9b020ef8957cba3ed541d67b2f5fde"
+  url "https://github.com/go-swagger/go-swagger/releases/download/v0.21.0/swagger_darwin_amd64"
+  version "0.21.0"
+  sha256 "ed3b2fcc0eb506bcb66d3e43322d2de4f6b555ef41659e64b6e4bcb555be26ec"
 
   option "with-goswagger", "Names the binary goswagger instead of swagger"
 
